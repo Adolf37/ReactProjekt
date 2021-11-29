9ae24727612d2c1b9c505d6256ad07b0b5fa3af0
Balint Adolf <adolfadolfbalint@gmail.com>
2021-11-29 13:43:54 +0200

    allapot megadasa az inputokkal

--- src/komponensek/AddTask.js
+++ src/komponensek/AddTask.js
@@ -1,20 +1,36 @@
+import {useState} from 'react'
+
 
 const AddTask = () => {
+
+    const [text,setText] =useState('')
+    const [day,setDay] =useState('')
+    const [emlek,setEmlek] =useState(false)
+
     return (
         <form className = 'add-form'>
             <div className='form-control'>
                 <label>Esemény</label>
-                <input type="text" placeholder='adj hozza valamit' />
+                <input type="text" 
+                    value ={text}
+                    onChange = {(e)=> setText(e.target.value)}
+                    placeholder='esemeny megadas'/>
             </div>
 
             <div className='form-control'>
                 <label>Időpont</label>
-                <input type="text" placeholder='idopont megadasa' />
+                <input type="text" placeholder='idopont megadasa'
+                    value={day}
+                    onChange = {(e)=> setDay(e.target.value)}
+                 />
             </div>
 
             <div className='form-control form-control-check'>
                 <label>Emlékeztető</label>
-                <input type="checkbox" />
+                <input type="checkbox"
+                    value={emlek}
+                    onChange = {(e)=> setEmlek(e.currentTarget.checked)}
+                     />
             </div>
             <input type="submit" value="Mentes" className='btn btn-block'/>
         </form>
